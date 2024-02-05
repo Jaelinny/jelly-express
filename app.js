06c62496f1b52f4ec422a80a18319771d6e73656
jaelinny <leenlyn3127@gmail.com>
2024-02-05 12:49:59 +0900

    update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	let myJson = { "Hello": "World"}
+	res.json(myJson)
+	// res.send('Hello World!')
 	})
 
 app.listen(port, () => {
